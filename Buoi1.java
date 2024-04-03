2b160f5b7e1d1b3ec4e332f7d329cd82b0ca9963
BongLanTrungMuoiNhieuChaBong <161852417+BongLanTrungMuoiNhieuChaBong@users.noreply.github.com>
2024-04-03 22:39:19 +0700

    .

--- Buoi1.java
+++ Buoi1.java
@@ -1,13 +1,20 @@
 public class Buoi1 {
     public static void main(String[] args) {
        //1
-        System.out.println( " xin chao cac ban");
+//        System.out.println( " xin chao cac ban");
+        //2
+//        hello( x :"Minh Anh");
+
         //4,5
-        int a = 4;
-        int b = 6;
-        int sum = a + b;
-        int tich = a*a;
-        System.out.println(" tong cua " +a+ " va "+b+ "la:" + sum);
-        System.out.println(" binh phuong cua " +a+ " la:" + tich);
+//        int a = 4;
+//        int b = 6;
+//        int sum = a + b;
+//        int tich = a*a;
+//        System.out.println(" tong cua " +a+ " va "+b+ "la:" + sum);
+//        System.out.println(" binh phuong cua " +a+ " la:" + tich);
+
     }
-}
+//    public void hello(String x ){
+//        x = "minh anh";
+//        System.out.println(" xin chao : " + x);
+    }
