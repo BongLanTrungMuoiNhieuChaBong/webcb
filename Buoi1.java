2abd53b0f9b804d2a14bf8530a4f8eec66359cce
BongLanTrungMuoiNhieuChaBong <161852417+BongLanTrungMuoiNhieuChaBong@users.noreply.github.com>
2024-04-03 22:19:23 +0700

    Initial commit

---
+++ Buoi1.java
@@ -0,0 +1,13 @@
+public class Buoi1 {
+    public static void main(String[] args) {
+       //1
+        System.out.println( " xin chao cac ban");
+        //4,5
+        int a = 4;
+        int b = 6;
+        int sum = a + b;
+        int tich = a*a;
+        System.out.println(" tong cua " +a+ " va "+b+ "la:" + sum);
+        System.out.println(" binh phuong cua " +a+ " la:" + tich);
+    }
+}
